fe724e1fbf3ee316a364863777c8d5a99b0450dc
Divesh kumar yadav <diveshyadav2298@gmail.com>
2025-10-26 21:55:27 +0530

    UI design

--- React js/06-tailwindcss/src/components/page2/Page2.jsx
+++ React js/06-tailwindcss/src/components/page2/Page2.jsx
@@ -6,7 +6,7 @@ const Page2 = (props) => {
   return (
     <div className='bg-[#e6cdb2] h-[40vh] w-full px-20'>
         <Top />
-        <div id='Fcard' className='flex flex-nowrap gap-20 overflow-scroll'> 
+        <div id='Fcard' className='flex flex-nowrap gap-10 overflow-scroll'> 
       {props.Coffee.map(function(elem){
        return <Fcard img={elem.img} about={elem.about} type={elem.type}/> 
       })}
