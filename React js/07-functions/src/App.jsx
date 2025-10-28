4da719ebb6c080162ea57b4bdd7b1d53bbe8c2ec
Divesh kumar yadav <diveshyadav2298@gmail.com>
2025-10-28 21:24:35 +0530

    functions

--- React js/07-functions/src/App.jsx
+++ React js/07-functions/src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
 const App = () => {
+  function clicked(){
+    console.log('Clicked');
+  }
+  let Move = (val)=>{
+    console.log(val)
+  }
   return (
-    <div>
-      apps
+    <div >
+      <div>
+      <button onClick={clicked}>Click here</button>
+      <div onMouseMove={Move} className='h-40 w-40 bg-amber-900'>vaygvyvy</div>
+      </div>
+      <div>
+        <input onChange={function(elem){
+          Move(elem.targer.input)
+        }} type="text" placeholder='enter' />
+      </div>
     </div>
   )
 }
