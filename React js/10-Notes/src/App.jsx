8fb778635005bef72a6fd1f5adbbadc93cbed3ae
Divesh kumar yadav <diveshyadav2298@gmail.com>
2025-10-31 21:06:01 +0530

    notes app

--- React js/10-Notes/src/App.jsx
+++ React js/10-Notes/src/App.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='h-screen bg-black text-white'>
+      <form className='rounded flex flex-col w-1/3 py-10 px-10 gap-7'>
+        <input 
+        type="text"
+        placeholder='Enter Notes Heading'
+        className='border-amber-50 border-2 py-2 px-4 text-lg'
+        />
+        <textarea
+         type='text' 
+         placeholder='Write details Here'
+         className='border-amber-50 border-2 py-2 px-4 h-50 text-lg'
+         >
+         </textarea>
+         <button className='bg-white text-black px-4 py-2'>Add Note</button>
+      </form>
     </div>
   )
 }
