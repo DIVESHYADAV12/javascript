0a647493ae3f53f8e5521864d79586dbb80f027f
Divesh kumar yadav <diveshyadav2298@gmail.com>
2025-11-04 13:07:17 +0530

    gallery project

--- React js/12-Gallery_project/src/App.jsx
+++ React js/12-Gallery_project/src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import axios from 'axios'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+   const [userdata,setUserdata]=useState([])
+   const Getdata = async ()=>{
+    const responce = await axios.get(`https://picsum.photos/v2/list?page=2&limit=30`)
+    setUserdata(responce.data)
+    console.log(userdata);
 
+    let printdefault = 'No Data Available'
+
+   }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <button
+       className='text-2xl m-4 active:scale-95 bg-black text-white rounded px-4 py-2'
+       onClick={Getdata}
+       >Get data</button>
+
+       <div>{printdefault}</div>
+    </div>
   )
 }
 
